d618e3a7db4a6e7928163d3f31327956a9f616aa
MauroAlde <48041709+Alexandryto@users.noreply.github.com>
2023-01-12 22:11:59 -0300

    BackEnd casi terminado

---
+++ src/main/java/com/portfolioweb/maa/Repository/RExperiencia.java
@@ -0,0 +1,14 @@
+
+package com.portfolioweb.maa.Repository;
+
+import com.portfolioweb.maa.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer>{
+    public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
---
+++ src/main/java/com/portfolioweb/maa/Service/PService.java
@@ -0,0 +1,46 @@
+package com.portfolioweb.maa.Service;
+
+import com.portfolioweb.maa.Entity.Persona;
+import com.portfolioweb.maa.Repository.PersonaRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class PService {
+    
+    @Autowired
+    PersonaRepository personaRepository;
+    
+    public List<Persona> list(){
+        return personaRepository.findAll();
+    }
+    
+    public Optional<Persona> getOne(int id){
+        return personaRepository.findById(id);
+    }
+    
+    public Optional<Persona> getByNombre(String nombre){
+        return personaRepository.findByNombre(nombre);
+    }
+    
+    public void save(Persona persona){
+        personaRepository.save(persona);
+    }
+    
+    public void delete(int id){
+        personaRepository.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return personaRepository.existsById(id);
+    }
+     
+    public boolean existsByNombre(String nombre){
+        return personaRepository.existsByNombre(nombre);
+    }
+    
+}
